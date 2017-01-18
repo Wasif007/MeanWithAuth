9fed550a6d13a7009acd2c23eefa0fa2c2f03bec
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-01-17 23:10:06 -0800

    model methods defined

--- app_api/models/user.js
+++ app_api/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-
+var crypto=require('crypto');
+var jwt=require('jsonwebtoken');
 var loginSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {
@@ -10,5 +11,26 @@ var loginSchema = new mongoose.Schema({
     hash:String,
     salt:String
 });
+
+loginSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+loginSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+loginSchema.methods.generatingJsonWebToken=function(){
+	var expairyData=new Date();
+	expairyData.setDate(expairyData.getDate()+7);
+	return jwt.sign({
+		_id:this._id,
+		email:this.email,
+		name:this.name,
+		exp: parseInt(expairyData.getTime() / 1000),
+	},"I am a Secret");
+}
 
 mongoose.model('User', loginSchema);
