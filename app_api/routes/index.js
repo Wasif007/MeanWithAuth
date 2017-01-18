840e99c0aefcf99403105ef2c08d4e14e714a36f
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-01-18 01:08:07 -0800

    adding middleware

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,13 +3,17 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlReviews = require('../controllers/reviews');
 var ctrlValidation=require('../controllers/validation');
-
+var jwt=require('express-jwt');
+var auth=jwt({
+secret:process.env.JWT_SECRET,
+userProperty:'payload'
+});
 
 router.get('/locations', ctrlLocations.locationsListByDistance);
-router.post('/locations', ctrlLocations.locationsCreate);
+router.post('/locations',auth, ctrlLocations.locationsCreate);
 router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+router.put('/locations/:locationid', auth,ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationid', auth,ctrlLocations.locationsDeleteOne);
 
 // reviews
 router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
